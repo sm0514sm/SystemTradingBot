6fd92376bbcb29082217f987c982fe5bb855e06c
sangminLee <sm0514sm@ajou.ac.kr>
2021-12-26 20:41:44 +0900

    fix: reporter 라인 겹치는 문제 해결

--- util/Reporter.py
+++ util/Reporter.py
@@ -15,7 +15,6 @@ class Reporter:
         self.logger = logging.getLogger("SystemLogger")
         matplotlib.rcParams['font.family'] = 'Malgun Gothic'
 
-
     def get_report_txt_file(self, mode):
         report_txt = "daily_report.txt"
         if not os.path.isfile(f'{root_path()}/{report_txt}'):
@@ -29,10 +28,8 @@ class Reporter:
         f.close()
 
     def make_daily_report(self) -> str:
-        f = self.get_report_txt_file("r")
-        daily_log_list = [(day_log.split(",")[0], int(day_log.split(",")[1]))
-                          for day_log
-                          in f.read().splitlines()]
+        daily_log_list = self.read_daily_log_list()
+
         dailys = list(map(list, zip(*daily_log_list)))
         fig, ax = plt.subplots(figsize=(14, 7))
         ax.plot(dailys[0], dailys[1], marker='o', markersize=5)
@@ -42,7 +39,6 @@ class Reporter:
         first_day = dailys[0][0]
         last_day = dailys[0][-1]
         filename = f'{root_path()}/daily_report/{first_day}-{last_day}.png'
-        f.close()
 
         plt.style.use('seaborn-pastel')
         plt.xlabel('날짜')
@@ -55,10 +51,18 @@ class Reporter:
         plt.tick_params(axis='x', rotation=90)
         plt.grid(True, axis='y', color='gray', alpha=0.5, linestyle='--')
         plt.tick_params(axis='both', direction='in')
-        plt.rcParams['xtick.major.size'] = 10000
         plt.savefig(filename, dpi=200)
         plt.show()
+
         return filename
+
+    def read_daily_log_list(self):
+        f = self.get_report_txt_file("r")
+        daily_log_list = [(day_log.split(",")[0], int(day_log.split(",")[1]))
+                          for day_log
+                          in f.read().splitlines()]
+        f.close()
+        return daily_log_list
 
 
 if __name__ == "__main__":
