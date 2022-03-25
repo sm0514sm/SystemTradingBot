fc74d5d31ccbd0f4dc04a348569a06453a568c0e
sangminLee <sm0514sm@ajou.ac.kr>
2022-03-25 19:44:57 +0900

    feat: update readme, sell_all_coins 수정

--- sell_all_coins.py
+++ sell_all_coins.py
@@ -1,33 +1,10 @@
-import uuid
-import jwt
-import requests
-import configparser
-from function.order_stock import sell_stock
+from object.Coin import Coin
 from trading_connector.CoinTradingConnector import CoinTradingConnector
-
-
-def get_account() -> list:
-    payload = {
-        'access_key': access_key,
-        'nonce': str(uuid.uuid4()),
-    }
-
-    jwt_token = jwt.encode(payload, secret_key).decode('utf-8')
-    authorize_token = 'Bearer {}'.format(jwt_token)
-    headers = {"Authorization": authorize_token}
-
-    res = requests.get('https://api.upbit.com/v1/accounts', headers=headers)
-
-    return res.json()
-
 
 if __name__ == "__main__":
     connector = CoinTradingConnector()
-    access_key = connector.access
-    secret_key = connector.secret
-    # 계정 내 모든 코인 팔기
-    for account in get_account():
-        if account['currency'] in ['KRW']:
-            continue
-        sell_stock("KRW-" + account['currency'], account['balance'], sleep=0.2)
-        print(account)
+    for _ in range(3): # 왠지 모르게 3번은 반복해야 모두 삭제됨
+        for coin in connector.get_balances():
+            print(coin.get('currency'))
+            print(coin.get('balance'))
+            connector.upbit.sell_market_order(f"KRW-{coin.get('currency')}", coin.get('balance'))
